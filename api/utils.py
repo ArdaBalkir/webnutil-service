c8d2cf2367754188b76cff38a99d6fd3a0ed3132
Arda Balkir <abalkir2014@gmail.com>
2025-08-22 12:20:55 +0200

    rollback w/o images

--- api/utils.py
+++ api/utils.py
@@ -282,3 +282,4 @@ async def upload_directory(directory_path: str, upload_base_path: str, token: st
                                     status_code=upload_response.status,
                                     detail=f"Failed to upload {relative_path}",
                                 )
+                print(f"✓ Uploaded {relative_path} to {upload_base_path}")
